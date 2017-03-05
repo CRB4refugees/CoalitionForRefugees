b6f6cb55c70769ee8fa00d1a6718ac6e87d0d391
BrotherDonkey <williambjorn@Ghosts-Pro.local>
2017-03-04 18:13:33 -0800

    Added mobile only navigation link, and added icon assets.

--- js/sticky_nav.js
+++ js/sticky_nav.js
@@ -5,24 +5,22 @@ $(window).scroll(function() {
 if (window.innerWidth  > 500){ //requires that the window is larger than iPad to work
 
   const focalImg = $('.focalImg');
-  console.log(focalImg);
+  // console.log(focalImg);
   const imgHeight = focalImg.height();
-  console.log(imgHeight);
+  // console.log(imgHeight);
 
 
 if ($(this).scrollTop() > imgHeight){
-    console.log(true)
-    console.log(`This is the window height: ${window.innerHeight} and this is the scroll height: ${$(this).scrollTop()}`)
+    // console.log(true)
+    // console.log(`This is the window height: ${window.innerHeight} and this is the scroll height: ${$(this).scrollTop()}`)
     $('nav').addClass("sticky");
-    console.log('stickified')
+    // console.log('stickified')
   }
   else{
-    console.log(false);
-    console.log(`This is the window height: ${window.innerHeight} and this is the scroll height: ${$(this).scrollTop()}`)
-
+    // console.log(false);
+    // console.log(`This is the window height: ${window.innerHeight} and this is the scroll height: ${$(this).scrollTop()}`)
     $('nav').removeClass("sticky");
-
-    console.log('sticky removed')
+    // console.log('sticky removed')
   }
 
 }//end if (window.innerHeight)
