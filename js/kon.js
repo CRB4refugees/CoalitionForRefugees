b2f72273c9a8b593453763f5cd155b5afef1772e
BrotherDonkey <williambjorn@Ghosts-Pro.local>
2017-04-01 15:28:18 -0700

    Made attribution page.

--- js/kon.js
+++ js/kon.js
@@ -1,11 +1,12 @@
 const keyPressed = [];
-const devName = 'willbjorn';
+const devName = 'willbjorn!';
 
 
 window.addEventListener('keyup', (e) => {
   keyPressed.push(e.key);
   keyPressed.splice(-devName.length-1, keyPressed.length - devName.length);
   if (keyPressed.join('').includes(devName)) {
+    
     console.log('you did it! you found me!');
     //add code to display here
   }
